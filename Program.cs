453d0af9983671a65da5630e2e9d9675e67246b4
SolomatovS <SolomatovS@GMail.com>
2020-01-11 00:45:37 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mysql_replication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
