73a44f9321725cca6fb0e952bd4ac4cc49d35850
SolomatovS <SolomatovS@GMail.com>
2020-01-11 00:54:25 +0700

    second commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace mysql_replication
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new HostBuilder()
+                .ConfigureHostConfiguration(c => c.AddEnvironmentVariables())
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddEnvironmentVariables();
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    config.AddJsonFile($"appsettings.dev.json", optional: true, reloadOnChange: true);
+                    config.AddCommandLine(args);
+                })
+                .ConfigureLogging((hostContext, logging) =>
+                {
+                    logging.AddSerilog((new LoggerConfiguration()).ReadFrom.Configuration(hostContext.Configuration).CreateLogger(), dispose: true);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddOptions();
+                })
+                .Build();
+
+            using (host)
+            {
+                await host.StartAsync();
+
+                await host.WaitForShutdownAsync();
+
+                await host.StopAsync();
+            }
         }
     }
 }
